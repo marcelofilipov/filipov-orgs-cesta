3a8a2ae39b82d16bfd90ea20cc1ffb34ae080bce
Marcelo Filipov <marcelo.filipov@gmail.com>
2022-01-05 21:11:13 -0300

    Defined basket details

--- src/components/cesta/index.js
+++ src/components/cesta/index.js
@@ -1,13 +1,28 @@
 import React from "react"
-import { Image, Text } from 'react-native'
+import { Image, Text, View } from 'react-native'
 
 import styles from "./style" 
 
 import topo from '../../../assets/topo.png'
+import logo from '../../../assets/logo.png'
 
 export default function Cesta() {
     return <>
         <Image source={topo} style={styles.topo}/>
         <Text style={styles.titulo}>Detalhe da cesta</Text>
+
+        <View style={styles.cesta}>
+            <Text style={styles.nome}>Cesta de Verduras</Text>
+            <View style={styles.fazenda}>
+                <Image source={logo} style={styles.imagemFazenda}></Image>
+                <Text style={styles.nomeFazenda}>Jenny Jack Farm</Text>
+            </View>
+            <Text style={styles.descricao}>
+                Uma cesta com produtos selecionados
+                cuidadosamente da fazenda direito
+                para sua cozinha
+            </Text>
+            <Text style={styles.preco}>R$ 40,00</Text>
+        </View>
     </>
 }
--- src/components/cesta/style.js
+++ src/components/cesta/style.js
@@ -17,6 +17,41 @@ const styles = StyleSheet.create({
         color: '#fff',
         fontWeight: 'bold',
         padding: 16,
+    },
+    cesta: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+    },
+    nome: {
+        color: '#464646',
+        fontSize: 26,
+        lineHeight: 42,
+        fontWeight: 'bold',
+    },
+    fazenda: {
+        flexDirection: 'row',
+        paddingVertical: 12,
+    },
+    imagemFazenda: {
+        width: 32,
+        height: 32,
+    },
+    nomeFazenda: {
+        fontSize: 16,
+        lineHeight: 26,
+        marginLeft: 12,
+    },
+    descricao: {
+        color: '#a3a3a3',
+        fontSize: 16,
+        lineHeight: 26,
+    },
+    preco: {
+        color: "#2a9f85",
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 42,
+        marginTop: 8,
     }
 })
 
